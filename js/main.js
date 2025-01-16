2e8120b508404aba59ca78fd3fc3a449484dedea
Oskar Nobis <oskarnobis@MacBook-Pro-Oskar.local>
2025-01-16 20:19:46 +0100

    wedding website

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
-const menuBtn = document.querySelector(`.burger-menu`);
-const nav = document.querySelector(`.nav`);
+const menuBtn = document.querySelector('.burger-menu');
+const nav = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav-items');
 
-function showNavigation() {
+function toggleNavigation() {
 	nav.classList.toggle('nav-show');
 }
-menuBtn.addEventListener('click', showNavigation);
+
+function closeNavigation() {
+	if (nav.classList.contains('nav-show')) {
+		nav.classList.remove('nav-show');
+	}
+}
+
+menuBtn.addEventListener('click', event => {
+	event.stopPropagation();
+	toggleNavigation();
+});
+
+navLinks.forEach(link => {
+	link.addEventListener('click', closeNavigation);
+});
+document.addEventListener('click', event => {
+	if (!nav.contains(event.target) && !menuBtn.contains(event.target)) {
+		closeNavigation();
+	}
+});
